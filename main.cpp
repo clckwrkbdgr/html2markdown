1b4ba6ca98d89b08ac0fccd3bc7ef7861151e00a
Igor Chaika <kp580bm1@gmail.com>
2014-06-27 12:16:35 +0300

    Reading HTML data from file.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include "src/html2mark.h"
 #include <iostream>
+#include <fstream>
 
-int main()
+int main(int argc, char ** argv)
 {
-	std::cout << Html2Mark::html2mark(std::cin,
-			Html2Mark::UNDERSCORED_HEADINGS | Html2Mark::MAKE_REFERENCE_LINKS);
+	if(argc > 1) {
+		std::ifstream in(argv[1]);
+		if(in.fail()) {
+			std::cerr << "Cannot open file \"" << argv[1] << "\"!" << std::endl;
+			return 1;
+		}
+		std::cout << Html2Mark::html2mark(in,
+				Html2Mark::UNDERSCORED_HEADINGS | Html2Mark::MAKE_REFERENCE_LINKS);
+	} else {
+		std::cout << Html2Mark::html2mark(std::cin,
+				Html2Mark::UNDERSCORED_HEADINGS | Html2Mark::MAKE_REFERENCE_LINKS);
+	}
 	return 0;
 }
