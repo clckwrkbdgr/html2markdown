a81796f0c880c5c912c713ed85a63b46e8409f91
Igor Chaika <kp580bm1@gmail.com>
2014-07-15 12:04:24 +0300

    Coloured output as an option for main app. Getopts.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,42 @@
 #include "src/html2mark.h"
+#include <getopt.h>
 #include <iostream>
 #include <fstream>
 
 int main(int argc, char ** argv)
 {
-	if(argc > 1) {
-		std::ifstream in(argv[1]);
+	int options = Html2Mark::UNDERSCORED_HEADINGS | Html2Mark::MAKE_REFERENCE_LINKS;
+	std::string filename;
+
+	static struct option long_options[] = {
+		{"color", no_argument, nullptr, 'c'},
+		{nullptr, 0, nullptr, 0}
+	};
+	while(true) {
+		int option_index = 0;
+		int c = getopt_long(argc, argv, "c", long_options, &option_index);
+		if(c == -1) {
+			break;
+		}
+		switch(c) {
+			case 'c': options |= Html2Mark::COLORS; break;
+			case '?': break;
+			default: return 1;
+		}
+	}
+	if(optind < argc) {
+		filename = argv[optind];
+	}
+
+	if(!filename.empty()) {
+		std::ifstream in(filename);
 		if(in.fail()) {
-			std::cerr << "Cannot open file \"" << argv[1] << "\"!" << std::endl;
+			std::cerr << "Cannot open file \"" << filename << "\"!" << std::endl;
 			return 1;
 		}
-		std::cout << Html2Mark::html2mark(in,
-				Html2Mark::UNDERSCORED_HEADINGS | Html2Mark::MAKE_REFERENCE_LINKS);
+		std::cout << Html2Mark::html2mark(in, options);
 	} else {
-		std::cout << Html2Mark::html2mark(std::cin,
-				Html2Mark::UNDERSCORED_HEADINGS | Html2Mark::MAKE_REFERENCE_LINKS);
+		std::cout << Html2Mark::html2mark(std::cin, options);
 	}
 	return 0;
 }
