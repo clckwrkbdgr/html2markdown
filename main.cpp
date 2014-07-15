3fc2ebee3c8a253dae223faaacd340a190110a78
Igor Chaika <kp580bm1@gmail.com>
2014-07-15 22:32:00 +0300

    Width option in main app.

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@
 int main(int argc, char ** argv)
 {
 	int options = Html2Mark::UNDERSCORED_HEADINGS | Html2Mark::MAKE_REFERENCE_LINKS;
+	size_t wrap_width = 0;
 	std::string filename;
 
 	static struct option long_options[] = {
 		{"color", no_argument, nullptr, 'c'},
+		{"width", required_argument, nullptr, 'w'},
 		{nullptr, 0, nullptr, 0}
 	};
 	while(true) {
@@ -20,6 +22,15 @@ int main(int argc, char ** argv)
 		}
 		switch(c) {
 			case 'c': options |= Html2Mark::COLORS; break;
+			case 'w': {
+				wrap_width = strtoul(optarg, nullptr, 10);
+				if(wrap_width <= 0) {
+					std::cerr << "Width must be greater than 0.\n";
+					return 1;
+				}
+				options |= Html2Mark::WRAP;
+				break;
+			}
 			case '?': break;
 			default: return 1;
 		}
@@ -34,9 +45,9 @@ int main(int argc, char ** argv)
 			std::cerr << "Cannot open file \"" << filename << "\"!" << std::endl;
 			return 1;
 		}
-		std::cout << Html2Mark::html2mark(in, options);
+		std::cout << Html2Mark::html2mark(in, options, 20, wrap_width);
 	} else {
-		std::cout << Html2Mark::html2mark(std::cin, options);
+		std::cout << Html2Mark::html2mark(std::cin, options, 20, wrap_width);
 	}
 	return 0;
 }
